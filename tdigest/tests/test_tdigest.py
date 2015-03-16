68a9d48ce4c451d3d3a48ad0900361dae44c7351
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-03-16 10:25:31 -0400

    dir restructure:

---
+++ tdigest/tests/test_tdigest.py
@@ -0,0 +1,52 @@
+import pytest
+
+from .tdigest import TDigest, Centroid
+
+
+@pytest.fixture()
+def empty_tdigest():
+    return TDigest()
+
+@pytest.fixture()
+def example_centroids():
+    return [
+        Centroid(0.5, 1),
+        Centroid(1.1, 1),
+        Centroid(1.5, 1),
+    ]
+
+
+class TestTDigest():
+
+
+    def test_add_centroid(self, empty_tdigest, example_centroids):
+        empty_tdigest.C = example_centroids
+        new_centroid = Centroid(0.9, 1)
+        empty_tdigest._add_centroid(new_centroid)
+        assert empty_tdigest.C == [
+            Centroid(0.5, 1),
+            new_centroid,
+            Centroid(1.1, 1),
+            Centroid(1.5, 1),
+        ]
+
+        last_centroid = Centroid(10., 1)
+        empty_tdigest._add_centroid(last_centroid)
+        assert empty_tdigest.C == [
+            Centroid(0.5, 1),
+            new_centroid,
+            Centroid(1.1, 1),
+            Centroid(1.5, 1),
+            last_centroid
+        ]    
+
+    def test_compute_centroid_quantile(self, empty_tdigest, example_centroids):
+        empty_tdigest.C = example_centroids
+        empty_tdigest.n = 3 
+
+        print empty_tdigest._compute_centroid_quantile(example_centroids[0])
+        print empty_tdigest._compute_centroid_quantile(example_centroids[1])
+        print empty_tdigest._compute_centroid_quantile(example_centroids[2])
+        assert False
+
+

f58f3186f4c04a5af8c556c0bb6319db549869ba
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2017-02-20 11:14:13 -0500

    because of the new req. plus the API changing under the hood, update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 
 setup(name='tdigest',
-      version='0.4.1.0',
+      version='0.5.0g.0',
       description='T-Digest data structure',
       author='Cam Davidson-pilon',
       author_email='cam.davidson.pilon@gmail.com',
